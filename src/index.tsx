dd0828e054860f9801c6dba9612647038ec93ae5
pandaCC95 <tvxq995@163.com>
2022-04-14 22:55:39 +1000

    chore: install dev tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import {createRoot} from 'react-dom/client';
+import { loadDevTools } from "jira-dev-tool";
 
-const container = document.querySelector('#root')!; //! null check in TS
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+loadDevTools(()=> {
+  const container = document.querySelector('#root')!; //! null check in TS
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
